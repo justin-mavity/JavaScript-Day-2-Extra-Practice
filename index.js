b321ce83cca5d2113af4ab47c632745ba1cc7ed4
justin <justinmavity1021@gmail.com>
2020-08-27 17:12:06 -0500

    finished projeect

--- index.js
+++ index.js
@@ -1,70 +1,117 @@
 // Use Array Methods to solve the tasks below 🎉
 
-const toppings = ['Mushrooms ', 'Tomatoes', 'Eggs', 'Chili', 'Lettuce', 'Avocado', 'Chiles', 'Bacon', 'Pickles', 'Onions', 'Cheese'];
+const { array } = require( "yargs" );
+
+const toppings = [ 'Mushrooms ', 'Tomatoes', 'Eggs', 'Chili', 'Lettuce', 'Avocado', 'Chiles', 'Bacon', 'Pickles', 'Onions', 'Cheese' ];
 
 /*
 TASK 1 🚀
-// The customer would like to see the topping options, console log out each of the toppings one by one in the array above 
+// The customer would like to see the topping options, console log out each of the toppings one by one in the array above
 */
 
+toppings.forEach( function ( item ) {
+    console.log( item );
+} )
+console.log( '' );
 
-/*
-TASK 2 🚀
-// write a function that accepts an array and an item and removes that item from the array
-for example you no longer have any onions and need to remove it from the list of toppings
-Use .forEach() - hint - you will need to include the index in the callback
- */
+// /*
+// TASK 2 🚀
+// // write a function that accepts an array and an item and removes that item from the array
+// for example you no longer have any onions and need to remove it from the list of toppings
+// Use .forEach() - hint - you will need to include the index in the callback
+//  */
 
+function remove( arr, item ) {
+    let index = arr.indexOf( item );
+    if ( index > -1 ) {
+        arr.splice( index, 1 );
+    }
+    return arr;
+}
 
-
+console.log( remove( toppings, "Eggs" ) );
+console.log( '' );
 /*
 TASK 3 🚀
-// Sort the topping alphabetically and return them in a new array 
+// Sort the topping alphabetically and return them in a new array
 */
 
-
+const newToppings = [ ...toppings ].sort();
+console.log( newToppings );
 
 
 const vacations = [
-    {city: 'Toronto', country: 'Canada', region: 'North America', temperature: 86, beach: true, sea: false, wifi:'strong', hiking: false, overall_rating: 6,},
-    {city: 'Miami', country: 'United States', region: 'North America', temperature: 95, beach: true, sea: true, wifi:'strong', hiking: false, overall_rating: 8,},
-    {city: 'Tel Aviv', country: 'Israel', region: 'Middle East', temperature: 102, beach: true, sea: true, wifi:'strong', hiking: true, overall_rating: 10,},
-    {city: 'Istanbul', country: 'Turkey', region: 'Europe', temperature: 86, beach: false, sea: false, wifi:'medium', hiking: true, overall_rating: 7,},
-    {city: 'Bangkok', country: 'Thailand', region: 'South East Asia', temperature: 101, beach: false, sea: false, wifi:'medium', hiking: false, overall_rating: 5,},
-    {city: 'Lima', country: 'Peru', region: 'South America', temperature: 86, beach: false, sea: false, wifi:'weak', hiking: true, overall_rating: 7,},
-    {city: 'Muscat', country: 'Oman', region: 'Middle East', temperature: 105, beach: true, sea: true, wifi:'strong', hiking: true, overall_rating: 9,},
-    {city: 'Sydney', country: 'Australia', region: 'Oceania', temperature: 92, beach: true, sea: true, wifi:'strong', hiking: true, overall_rating: 8,},
-    {city: 'Cape Town', country: 'South Africa', region: 'Africa', temperature: 87, beach: true, sea: true, wifi:'medium', hiking: true, overall_rating: 10,},
-    {city: 'Cancun', country: 'Mexico', region: 'North America', temperature: 95, beach: true, sea: true, wifi:'medium', hiking: false, overall_rating: 7,},
+    { city: 'Toronto', country: 'Canada', region: 'North America', temperature: 86, beach: true, sea: false, wifi: 'strong', hiking: false, overall_rating: 6, },
+    { city: 'Miami', country: 'United States', region: 'North America', temperature: 95, beach: true, sea: true, wifi: 'strong', hiking: false, overall_rating: 8, },
+    { city: 'Tel Aviv', country: 'Israel', region: 'Middle East', temperature: 102, beach: true, sea: true, wifi: 'strong', hiking: true, overall_rating: 10, },
+    { city: 'Istanbul', country: 'Turkey', region: 'Europe', temperature: 86, beach: false, sea: false, wifi: 'medium', hiking: true, overall_rating: 7, },
+    { city: 'Bangkok', country: 'Thailand', region: 'South East Asia', temperature: 101, beach: false, sea: false, wifi: 'medium', hiking: false, overall_rating: 5, },
+    { city: 'Lima', country: 'Peru', region: 'South America', temperature: 86, beach: false, sea: false, wifi: 'weak', hiking: true, overall_rating: 7, },
+    { city: 'Muscat', country: 'Oman', region: 'Middle East', temperature: 105, beach: true, sea: true, wifi: 'strong', hiking: true, overall_rating: 9, },
+    { city: 'Sydney', country: 'Australia', region: 'Oceania', temperature: 92, beach: true, sea: true, wifi: 'strong', hiking: true, overall_rating: 8, },
+    { city: 'Cape Town', country: 'South Africa', region: 'Africa', temperature: 87, beach: true, sea: true, wifi: 'medium', hiking: true, overall_rating: 10, },
+    { city: 'Cancun', country: 'Mexico', region: 'North America', temperature: 95, beach: true, sea: true, wifi: 'medium', hiking: false, overall_rating: 7, },
 ];
+console.log( '' );
 
 /*
 TASK 4 🚀
-// The travel agent would like to send a couple on their honeymoon to a location with a beach and a temperature above 90 degrees. return their options in a new array 
+// The travel agent would like to send a couple on their honeymoon to a location with a beach and a temperature above 90 degrees. return their options in a new array
 */
 
-
+const options = [];
+vacations.forEach( function ( item ) {
+    if ( item.beach === true && item.temperature > 90 ) {
+        options.push( item.city );
+    }
+} )
+console.log( options );
+console.log( '' );
 
 /*
 TASK 5 🚀
 // A developer decides to become a digital nomad for a year, they would like to live in a place with strong wifi, a beach, and good hiking, return their options
 */
 
+const developerOptions = [];
+vacations.forEach( function ( item ) {
+    if ( item.beach === true && item.wifi === "strong" && item.hiking === true ) {
+        developerOptions.push( item.city );
+    }
+} )
+console.log( developerOptions );
 
-
-/* 
+/*
 TASK 6 🚀
 // write a function that allows a user to sort their vacations by hiking opportunities, beach access or a mix of both and return their options
 */
+function mySort( arr, val ) {
+    if ( val === 'beach' || val === ' hiking' ) {
+        if ( (arr.beach =  true) || ( arr.hiking = true)) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+    return arr;
+}
+
+console.log( mySort(vacations, 'beach' ) );
 
 
-
-/* 
+/*
 TASK 7 🚀
-// write a function that finds the average of overall ratings in a given array. The function should take an array as its argument and should return the average of the overall ratings in that array 
+// write a function that finds the average of overall ratings in a given array. The function should take an array as its argument and should return the average of the overall ratings in that array
 hint - use .reduce()
 */
 
+function average( arr ) {
+     const ave = vacations.reduce( function ( acc, item ) {
+        return acc + item.overall_rating;
+     }, 0 );
+    return ave;
+}
+console.log( average( vacations ) );
 
 /*
 TASK 8 🚀
@@ -72,3 +119,13 @@ Find the airport codes for each of the cities in the vacation array and write a 
 hint - your function should include array, index and code as parameters
 you will need to invoke the function each time you wish to add a new code
 */
+const newArr = [];
+function addCode( arr, index, code ) {
+    arr.airportCode = code;
+    if ( arr.includes( index ) ) {
+        arr[ i ].push( code );
+        newArr.push( [...arr, airportCode] );
+    }
+    console.log( newArr );
+}
+addCode( vacations, 'Toronto', 'YYZ' ) ;
